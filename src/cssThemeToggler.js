956c62f832e667eb81beaad180c27dc7e52720f5
John Niduelan <jeniduelan@gmail.com>
2024-08-31 10:45:56 +1000

    feat: saving and loading theme working

--- src/cssThemeToggler.js
+++ src/cssThemeToggler.js
@@ -1,7 +1,8 @@
 console.log("CSS Theme toggler loaded and running!");
 
+
 let themes = [
-    {
+	{
 		name: "dark",
 		properties: {
 			backgroundColour: "darkgrey",
@@ -13,8 +14,9 @@ let themes = [
 		name: "light",
 		properties: {
 			backgroundColour: "#87E0E0",
-			fontColour: "black",
-            "theme-100": "#4641d1"
+			fontColour: "black"
+			,
+			"theme-100": "#87E0E0"
 		}
 	}
 ];
@@ -22,31 +24,78 @@ let themes = [
 // Read theme name stored in local storage 
 // and update CSS variables based on that name 
 function getChosenTheme(){
-
+	let foundTheme = localStorage.getItem("theme");
+	console.log(foundTheme);
+	return foundTheme;
 }
 
 // Set theme name to local storage 
 // and update CSS variables based on that name 
 function setChosenTheme(newThemeName){
-
+	localStorage.setItem("theme", newThemeName);
+	updateCssVariables();
 }
+
+if (getChosenTheme() == null){
+	const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+	if (darkThemeMq.matches) {
+	  // Theme set to dark.
+	  setChosenTheme("dark");
+	  console.log("No theme found, applied the dark theme");
+	} else {
+	  // Theme set to light.
+	  setChosenTheme("light");
+	  console.log("No theme found, applied the light theme");
+
+	}
+}
+
+
+
+
+function toggleTheme(){
+	// if ("dark" == "dark"){
+	// if ("light" == "dark"){
+	if (getChosenTheme() == "dark"){
+		// set it to light 
+		setChosenTheme("light");
+	} else {
+		// set it to dark 
+		setChosenTheme("dark");
+	}
+}
+
+let themeToggleButton = document.getElementById("themeToggle");
+themeToggleButton.onclick = toggleTheme;
+// themeToggleButton.addEventListener("click", toggleTheme);
 
 // Loop through properties key in chosen theme object 
 // and apply those properties to CSS 
 function updateCssVariables(){
+	// Find the matching theme object
+	let matchingTheme = themes.find(themeObject => themeObject.name == getChosenTheme());
+	console.log(matchingTheme);
 
+	// Find the properties object in the matching theme object 
+
+	// Loop through all properties 
+
+	// Apply property value to CSS variables
 }
 
-let rootElement = document.querySelector(":root")
+
+let rootElement = document.querySelector(":root");
 
 function getVariablesFromCSS(){
-    console.log(rootElement);
+	console.log(rootElement);
 
-    // console.log(document.documentElement.style.getPropertyValue("--backgroundColour"));
+	// console.log(document.documentElement.style.getPropertyValue("--backgroundColour"));
 
-    let rootStyles = getComputedStyle(rootElement);
+	let rootStyles = getComputedStyle(rootElement);
 
-    console.log(rootStyles.getPropertyValue("--backgroundColour"));
+	console.log(rootStyles.getPropertyValue("--backgroundColour"));
+
 }
 
 getVariablesFromCSS();
+
